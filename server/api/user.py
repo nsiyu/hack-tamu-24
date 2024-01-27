9360efeb0199867dab4e34245c0e964ebd176e64
nsiyu <nsiyuhuang@gmail.com>
2024-01-27 14:44:10 -0600

    First Commit

--- server/api/user.py
+++ server/api/user.py
@@ -9,6 +9,10 @@ user_model = user_api.model('User', {
     'password': fields.String(required=True, description='User password'),
 })
 
+get_user_model = user_api.model('User', {
+    'user_id': fields.String(required=True, description='User Id'),
+})
+
 @user_api.route('/signup')
 class SignupResource(Resource):
     @user_api.doc('signup')
@@ -20,4 +24,17 @@ class SignupResource(Resource):
         username = data['username']
         password = data['password']
         db.user.insert_one({'username': username, 'password': password})
+        return {'message': 'User registered successfully'}, 201
+
+
+@user_api.route('/past-flights')
+class PastFlightsResource(Resource):
+    @user_api.doc(
+        description='Retrieve flights based on user ID',
+        params={'user_id': 'The ID of the user to fetch flights for'}
+    )
+    @user_api.response(201, 'Past Flights retrieved successfully')
+    def post(self):
+        db = get_db()
+        user_id = request.args.get('user_id')
         return {'message': 'User registered successfully'}, 201
