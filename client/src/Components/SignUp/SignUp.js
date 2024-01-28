2bb662e77fd2aa61b1e655b75e39eb0695a97ec8
nsiyu <nsiyuhuang@gmail.com>
2024-01-28 03:00:02 -0600

    First Commit

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -1,43 +1,75 @@
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Input } from 'antd';
 import './SignUp.css';
+import axios from "axios";
 
 const SignUpPage = () => {
-  return (
-    <div className="signup-page">
-      <div className="welcome-message">
-        <h1>Let's Get Started!</h1>
-        <p>Create an Account</p>
-      </div>
-      <div className="input-container">
-          <Input
-          placeholder="Email"
-          className="input-field"
-        />
-          <Input
-          placeholder="Email"
-          className="input-field"
-        />
-        <Input
-          placeholder="Email"
-          className="input-field"
-        />
-        <Input.Password
-          placeholder="Password"
-          className="input-field"
-        />
-        <p className="forgot-password-link">Forgot Password?</p>
-        <Button type="primary" shape="round" className="sign-in-btn" style={{ width: '350px' }}>
-          Sign in
-        </Button>
-      </div>
-      <div className="sign-up-link">
-        <p>Don't have an account? <a href="/signup" style={{ color: '#aaa' }}>Sign up</a></p>
-      </div>
-    </div>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const handleSignUp = () => {
+        axios.post('http://127.0.0.1:5000/users/signup', {
+            "fname": firstName,
+            "lname": lastName,
+            "email": email,
+            "password": password
+        })
+        .then(response => {
+            console.log('Success:', response.data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
+
+    return (
+        <div className="signup-page">
+            <div className="welcome-message">
+                <h1>Let's Get Started!</h1>
+                <p>Create an Account</p>
+            </div>
+            <div className="input-container">
+                <Input
+                    placeholder="First Name"
+                    className="input-field"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <Input
+                    placeholder="Last Name"
+                    className="input-field"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <Input
+                    placeholder="Email"
+                    className="input-field"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input.Password
+                    placeholder="Password"
+                    className="input-field"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button
+                    type="primary"
+                    shape="round"
+                    className="sign-in-btn"
+                    style={{ width: '350px' }}
+                    onClick={handleSignUp}
+                >
+                    Sign Up
+                </Button>
+            </div>
+            <div className="sign-up-link">
+                <p>Already have an account? <a href="/login" style={{ color: '#aaa' }}>Sign in</a></p>
+            </div>
+        </div>
+    );
 };
 
 export default SignUpPage;
